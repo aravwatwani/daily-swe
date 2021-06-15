71c6a19ca8f7d8378c518add50e0d04c7d85815c
Arav Watwani <aravwatwani@gmail.com>
2021-06-14 20:01:26 -0700

    parsing working

--- sdd.py
+++ sdd.py
@@ -5,3 +5,33 @@ import requests
 from selenium import webdriver
 from twilio.rest import Client
 
+data = []
+disclosures = []
+text_to_send = ''
+apple = False
+apple_string = ''
+
+# get list of URLs to visit and plunder!
+
+try:
+    url = 'https://github.com/pittcsc/Summer2022-Internships'
+    r = requests.get(url)
+    soup = bs(r.content, 'html.parser')
+
+     
+    table = soup.find('table')
+    table_body = table.find('tbody')
+    rows = table_body.find_all('tr')
+
+    for row in rows:
+        cols = row.find_all('td')
+        cols = [ele.text.strip() for ele in cols]
+        data.append({cols[0]: cols[-1]})
+    # print(cols)
+
+except:
+   text_to_send = '🚨 Failed on bs4 execution'
+   print(text_to_send)
+
+
+print(data)
